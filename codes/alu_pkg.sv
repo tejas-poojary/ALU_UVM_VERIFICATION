9cea5482cb03a3e293cb84cc2ce18c4100d144a5
tejas-poojary <tejaspoojary2003@gmail.com>
2025-08-22 10:43:03 +0530

    Create alu_pkg.sv

---
+++ codes/alu_pkg.sv
@@ -0,0 +1,15 @@
+package alu_pkg;
+ import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "alu_defines.sv"
+`include "alu_sequence_item.sv"
+`include "alu_sequence.sv"
+`include "alu_driver.sv"
+`include "alu_monitor.sv"
+`include "alu_sequencer.sv"
+`include "alu_scoreboard.sv"
+`include "alu_subscriber.sv"
+`include "alu_agent.sv"
+`include "alu_environment.sv"
+`include "alu_test.sv"
+endpackage
