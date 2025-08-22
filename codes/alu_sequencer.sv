a0f87808b72aade405d71ae4d66ca181860cb78a
tejas-poojary <tejaspoojary2003@gmail.com>
2025-08-22 10:51:27 +0530

    Create alu_sequencer.sv

---
+++ codes/alu_sequencer.sv
@@ -0,0 +1,8 @@
+class alu_sequencer extends uvm_sequencer#(alu_seq_item);
+  `uvm_component_utils(alu_sequencer)
+
+  function new(string name="alu_sequencer",uvm_component parent=null);
+    super.new(name,parent);
+  endfunction
+
+endclass
